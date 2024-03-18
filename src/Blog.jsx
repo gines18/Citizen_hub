afdce897fd4f69c31a93bab380347aa13884d05c
gines18 <ruczkowski.m@gmail.com>
2024-03-18 11:33:53 +0000

    change Article.jsx window size

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar";
 import { Link, Outlet } from "react-router-dom";
-
-
-
-
-
+import Footer from "./Footer";
 function Blog() {
   const blogs = [
     {
       id: 1,
-      title: "Why Every Company Needs a Website: Unlocking the Gateway to Digital Success",
+      title:
+        "Why Every Company Needs a Website: Unlocking the Gateway to Digital Success",
       body: "In today's digital age, having a website isn't just a luxury, it's a necessity for any company striving to thrive in the competitive business landscape. ",
       category: "Digital presence",
       Date: "17 March 2024",
@@ -24,7 +21,8 @@ function Blog() {
     },
     {
       id: 3,
-      title: "Why Hiring a Professional Web Designer is Essential for Your Business",
+      title:
+        "Why Hiring a Professional Web Designer is Essential for Your Business",
       body: "Hiring a professional web designer is essential for your business because they possess the expertise and experience needed to create a website that not only looks visually appealing but also functions seamlessly and effectively.",
       category: "Web Design",
       Date: "17 March 2024",
@@ -33,58 +31,55 @@ function Blog() {
 
   return (
     <>
-    <Navbar />
-   
-    <div>
-    {blogs.map((blog) => (
-      <section class="text-gray-600 body-font overflow-hidden">
-        <div class="container px-5 py-5 w-full ml-auto me-auto ">
-          <div class="-my-8 divide-y-2 divide-gray-100">
-            <div class="py-8 flex flex-wrap md:flex-nowrap">
-              <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-                <span class="font-semibold title-font text-gray-700">
-                  {blog.category}
-                </span>
-                <span class="mt-1 text-gray-500 text-sm">{blog.Date}</span>
-              </div>
-              <div key={blog.id} class="md:flex-grow">
-                <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
-                  {blog.title}
-                </h2>
-                <p class="leading-relaxed">
-                  {blog.body}
-                </p>
-                <Link
-                  to={`/blog/${blog.id}`}
-                  className="text-indigo-500 inline-flex items-center mt-4"
-                >
-                  Read More
-                  <svg
-                    class="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    fill="none"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M5 12h14"></path>
-                    <path d="M12 5l7 7-7 7"></path>
-                  </svg>
-                </Link>
+      <Navbar />
+
+      <div class="overflow-y-scroll h-article">
+        {blogs.map((blog) => (
+          <section class="text-gray-600 body-font  ">
+            <div class="container px-5 py-5  sm:w-full md:w-4/5 lg:w-4/5 ml-auto me-auto  ">
+              <div class="-my-8 divide-y-2 divide-gray-100">
+                <div class="py-8 flex flex-wrap md:flex-nowrap">
+                  <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+                    <span class="font-semibold title-font text-gray-700">
+                      {blog.category}
+                    </span>
+                    <span class="mt-1 text-gray-500 text-sm">{blog.Date}</span>
+                  </div>
+                  <div key={blog.id} class="md:flex-grow">
+                    <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
+                      {blog.title}
+                    </h2>
+                    <p class="leading-relaxed">{blog.body}</p>
+                    <Link
+                      to={`/blog/${blog.id}`}
+                      className="text-indigo-500 inline-flex items-center mt-4"
+                    >
+                      Read More
+                      <svg
+                        class="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        stroke-width="2"
+                        fill="none"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      >
+                        <path d="M5 12h14"></path>
+                        <path d="M12 5l7 7-7 7"></path>
+                      </svg>
+                    </Link>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-    ))}
-</div>
+          </section>
+        ))}
+      </div>
 
-    <div><Outlet /></div>
-    
-   
-  </>
-
+      <div>
+        <Outlet />
+      </div>
+    </>
   );
 }
 
